783aa5dcab3ff9368abc9656b34f2eef0cad4c0f
Sergey Ivashko <sergei.s.iv@gmail.com>
2022-06-08 23:16:30 +0300

    Skeleton of the app and working with database feature.

---
+++ API/Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿namespace API.Data
+{
+    using API.Entities;
+    using Microsoft.EntityFrameworkCore;
+
+    public class DataContext : DbContext
+    {
+        public DataContext (DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<AppUser> Users { get; set; }
+    }
+}
