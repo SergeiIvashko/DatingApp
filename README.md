cc56f88aa013f8ca20538fa8312add94c8387e93
Sergey Ivashko <sergei.s.iv@gmail.com>
2022-06-18 15:01:33 +0300

    Added frontend part for login and register of the users.

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # DatingApp
-ASP.Net &amp; Angular application.
+Hi! That is a learning application a goal of which create an app which provides to users a dating service. That is a part of course
+<a href="https://www.udemy.com/course/build-an-app-with-aspnet-core-and-angular-from-scratch/">Build an app with ASPNET Core and Angular from scratch</a>.
+Performing the app involves the following frameworks and technologies:
+ - .Net
+ - ASP&period;NET
+ - SQL Lite
+ - Entity Framework
+ - Angular.
+ 
+If you see this note that means I still work on the app and regularly upload new commits in the repository.
--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { User } from './_models/user';
+import { AccountService } from './_services/account.service';
 
 
 @Component({
@@ -12,16 +14,14 @@ export class AppComponent  implements OnInit{
   title = 'The Dating app';
   users: any;
 
-  constructor(private http: HttpClient) {}
+  constructor(private accountService: AccountService) {}
 
   ngOnInit() {
-    this.getUsers();
+    this.serCurrentUser();
   }
 
-  getUsers() {
-    this.http.get('https://localhost:5001/api/users').subscribe({
-      next: response => this.users = response,
-      error: error => console.log(error)
-    })
+  serCurrentUser() {
+    const user: User = JSON.parse(localStorage.getItem('user'));
+    this.accountService.setCurrentUser(user);
   }
 }
