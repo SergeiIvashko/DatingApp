50e87804971daaf89caafc265ac26ac80d09247e
Sergey Ivashko <sergei.s.iv@gmail.com>
2022-07-07 00:03:02 +0300

    Added Entity Framework relationships and Conventions, seeded users data to the database, implemented Repository pattern, implemented AutoMapper

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -1,15 +1,19 @@
-﻿using API.Data;
-using API.Interfaces;
-using API.Services;
-using Microsoft.EntityFrameworkCore;
-
-namespace API.Extensions
+﻿namespace API.Extensions
 {
+    using API.Data;
+    using API.Helpers;
+    using API.Interfaces;
+    using API.Services;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.Extensions.DependencyInjection;
+
     public static class ApplicationServiceExtensions
     {
         public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config)
         {
             services.AddScoped<ITokenService, TokenService>();
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddAutoMapper(typeof(AutoMapperProfiles).Assembly);
             services.AddDbContext<DataContext>(options =>
             {
                 options.UseSqlite(config.GetConnectionString("DefaulConnection"));
---
+++ API/Interfaces/IUserRepository.cs
@@ -0,0 +1,22 @@
+﻿namespace API.Interfaces
+{
+    using API.DTOs;
+    using API.Entities;
+
+    public interface IUserRepository
+    {
+        void Update(AppUser user);
+
+        Task<bool> SaveAllAsync();
+
+        Task<IEnumerable<AppUser>> GetUsersAsync();
+
+        Task<AppUser> GetUserByAsync(int id);
+
+        Task<AppUser> GetUserByUsernameAsync(string username);
+
+        Task<IEnumerable<MemberDto>> GetMembersAsync();
+
+        Task<MemberDto> GetMemberAsync(string username);
+    }
+}
